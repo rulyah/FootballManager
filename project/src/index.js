005783504364d9da6af59de1a1c2041324ce6cb2
rulyah <brulyah@gmail.com>
2020-07-10 16:42:53 +0300

    111

--- project/src/index.js
+++ project/src/index.js
@@ -1,9 +1,32 @@
-// const fs = require('fs')
+const mongoose = require('mongoose/browser');
 const path = require('path');
 import css from './index.css';
-import playerTemplate from "./players-list"
+import playerTemplate from "./players-list";
+import Player from './Schema';
+import * as name from "./players-list";
 
+// let mongoose = require('mongoose');
 
-// fs.writeFile("players.txt", "hello", (err) => {
-//     if(err) console.log('err')
-// })
+async function start(){
+    try {
+        await mongoose.connect(`mongodb+srv://ruslan:vami7up4uk@cluster0-bmnqp.mongodb.net/football?retryWrites=true&w=majority`, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useFindAndModify: false
+        });
+
+        console.log('[Mongoose] Connected!');
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+start();
+let res = () => player1.skillScore/(player1.skillScore+player2.skillScore)*100;
+
+function rand (min, max){
+    return Math.round( Math.random() * (max - min) + min );
+}
+let randomPersent = rand(0, 100);
+
+// console.log(skillChoosePlayer)
--- project/src/players-list.js
+++ project/src/players-list.js
@@ -5,24 +5,23 @@ document.addEventListener('click',function(e){
         let playersCollection = document.getElementsByClassName("player-radio");
         
         let players = Array.prototype.slice.call(playersCollection);
-        // console.log(`Players count: ${players.length}`);
 
         let activePlayers = players.filter(function(value, index, array) {
             return value.checked;
         });
 
-        // console.log(`Active players: ${activePlayers.length}`);
-
         if(activePlayers.length !== 1) {
             console.error("Choose One");
+
         } else {
-            // let idChoose = +activePlayers[0].value
-            // console.log(idChoose)
             let name = activePlayers[0].parentNode.parentNode.parentNode.getElementsByTagName("td")[1].innerHTML;
             console.log(`Yours choose is: ${name.replace(/\s+/g, ' ').trim()}`);
-
-            // let pl = json.players.find(element => element.idChoose)
-            // console.log(pl)
+            
+            let idChoose = +activePlayers[0].value
+            console.log(`id chosen player: ${idChoose}`)
+    
+            let skillChoosePlayer = json.players[`${idChoose-1}`].skillScore
+            console.log(`Skill score chosen player: ${skillChoosePlayer}`)
         }
     }
  });
@@ -75,9 +74,38 @@ function getPlayersTable() {
     return html;
 }
 
+document.addEventListener('click',function(e){
+    if(e.target && e.target.id== 'create') {
+        event.preventDefault()
+        let nameField = document.getElementById("name").value;
+        let surnameField = document.getElementById("surname").value;
+        let skillField = document.getElementById("skill").value;
+        let url = document.getElementById("url").value;
+        let res = `First name: ` + nameField +` `+ `Last name: `+surnameField + ' ' + `Skill:` +skillField;
+        
+        console.log(res);
+    }
+})
+
+document.getElementById("form").innerHTML = `
+    <h1> Create your player <br> or select from the list </h1>
+
+        <form action="#" method="GET">
+        Имя: <input type="text" id="name" value=""/>
+        <br>
+        Фамилия: <input type="text" id="surname" value=""/>
+        <br>
+        Навык: <input type="number" id="skill" value=""/>
+        <br>
+        Фото: <input type="url" id="url" value=""/>
+        <br>
+
+        Пгнали? <button id="create" name="GO">Create!</button>
+        <hr>
+        </form>
+`;
 document.getElementById("app").innerHTML = `
-    <h1 class="app-title"> We have ${json.players.length} players </h1>
+    <h2> We have ${json.players.length} players </h2>
     ${getPlayersTable()}
     <button class="btn-small" type="submit" id="submit">Вперде!</button>
-    
-`
+`;
